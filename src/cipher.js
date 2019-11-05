ee295e6320dc4c158c6a36a22501fd9240886c4f
Nicolas Roman <nicolas.roman@nidarbox.com>
2019-11-05 09:43:48 -0300

    Subo Proyecto Cifrado Cesar completo

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,35 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (valueTextArea, valueInputNumber) => {
+    var newString = ''
+    
+    for(let i=0; i<valueTextArea.length; i++) {
+      var x = valueTextArea.charCodeAt(i)
+      if (x !== 32) {
+          var resultado = (x - 65 + parseInt(valueInputNumber)) % 26 + 65
+          newString = newString + String.fromCharCode(resultado)
+        } 
+      else {
+          newString = newString + String.fromCharCode(x)
+      }
+  }
+
+    
+    return newString
     
   },
-  decode: () => {
-    /* Acá va tu código */
+  decode: (valueTextArea, valueInputNumber) => {
+    var newString = ''
+    for(let i=0; i<valueTextArea.length; i++) {
+      var x = valueTextArea.charCodeAt(i) 
+      if (x !== 32) {
+        var resultado = (x - 90 - parseInt(valueInputNumber)) % 26 + 90
+        newString = newString + String.fromCharCode(resultado)
+      }
+      else {
+          newString = newString + String.fromCharCode(x)
+      }
+    }
+
+    return newString
   }
-};
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-/*Acá va tu código*/
+document.getElementById("codificar").addEventListener("click",setCipher);
+document.getElementById("decodificar").addEventListener("click",setCipher);
+document.getElementById("submit").addEventListener("click",submit);
+
+function setCipher(e) {
+    let id = e.target.id;
+
+    let inputText = document.getElementById("inputtext")
+    inputText.style.visibility = "visible"
+
+    let instruction = document.getElementById("instruction");
+    instruction.innerHTML = "Ingresa tu mensaje a " + id
+
+    let contentButton = id == "codificar" ? "CODIFÍCAME!" : "DECODIFÍCAME!"
+    let submitButton = document.getElementById("submit")
+    submitButton.innerHTML = contentButton
+}
+
+function submit(e) {
+    let action = e.target.innerText == "CODIFÍCAME!" ? "encode" : "decode"
+    
+    let valueTextArea = document.getElementById("textarea").value;
+    let valueInputNumber = document.getElementById("inputnumber").value;
+    let result = document.getElementById("result");
+    let tituloresultado = document.getElementById("tituloresultado");
+
+    if (action === "encode" ) {
+        let resultado = cipher.encode(valueTextArea, valueInputNumber)
+        result.innerHTML = resultado
+        result.style.visibility = "visible"
+        tituloresultado.style.visibility = "visible"
+    } else {
+        let resultado = cipher.decode(valueTextArea, valueInputNumber)
+        result.innerHTML = resultado
+        result.style.visibility = "visible"
+        tituloresultado.style.visibility = "visible"
+    }
+}
